2633012bf2497c2c1346b5f7eecec1ae8e3ef571
GhostX <egamovkhurshed29@gmail.com>
2020-04-08 18:27:43 +0500

    Corrected

--- Program.cs
+++ Program.cs
@@ -6,11 +6,38 @@ namespace ArrayHelper
     {
         static void Main(string[] args)
         {
-            string[] x = { "Asd", "Sds" };
-            string end = _ArrayHelper_.Pop(ref x);
-            _ArrayHelper_.Push(ref x,"NE");
-            string fs = _ArrayHelper_.Shift(ref x);
-            _ArrayHelper_.UnShift(ref x,"ne");
+            int[] x = { 1, 2, 3, 4 };
+            System.Console.WriteLine("----List init-----");
+            foreach (var z in x)
+            {
+                System.Console.WriteLine(z);
+            }
+            System.Console.WriteLine("----List after Pop method-----");
+            int end = _ArrayHelper_.Pop(ref x);
+            foreach (var z in x)
+            {
+                System.Console.WriteLine(z);
+            }
+            System.Console.WriteLine($"deleted {end}");
+            System.Console.WriteLine("----List after Push method-----");
+            _ArrayHelper_.Push(ref x, 6);
+            foreach (var z in x)
+            {
+                System.Console.WriteLine(z);
+            }
+            System.Console.WriteLine("----List after Shift method-----");
+            int fs = _ArrayHelper_.Shift(ref x);
+            foreach (var z in x)
+            {
+                System.Console.WriteLine(z);
+            }
+            System.Console.WriteLine($"deleted {fs}");
+            System.Console.WriteLine("----List after UnShift method-----");
+            _ArrayHelper_.UnShift(ref x, 8);
+            foreach (var z in x)
+            {
+                System.Console.WriteLine(z);
+            }
             Console.ReadKey();
         }
     }
